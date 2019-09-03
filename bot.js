911cda1357df5f010c521fa82149961886f3fa0a
Thomas Helton <thomashelton24@gmail.com>
2019-09-03 09:04:04 -0400

    Removed .meme command

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
-const snekfetch = require('snekfetch');
 
 require('events').EventEmitter.defaultMaxListeners = 100;
 
@@ -29,7 +28,7 @@ var food4 = myArray[Math.floor(Math.random()*myArray.length)];
 
 
 
-client.on('ready', () => { client.user.setGame('Use .help for help!'); });
+client.on('ready', () => { client.user.setGame('Use .help for help'); });
 
 
 
@@ -170,29 +169,6 @@ client.on('message', message => {
 });
 
 
-
-exports.run = async ('message', message => {
-  if (message.content === ".meme") {
-    try {
-        const { body } = await snekfetch
-            .get('https://www.reddit.com/r/dankmemes.json?sort=top&t=week')
-            .query({ limit: 800 });
-        const allowed = message.channel.nsfw ? body.data.children : body.data.children.filter(post => !post.data.over_18);
-        if (!allowed.length) return message.channel.send('It seems we are out of fresh memes!, Try again later.');
-        const randomnumber = Math.floor(Math.random() * allowed.length)
-        const embed = new Discord.RichEmbed()
-        .setColor(0x00A2E8)
-        .setTitle(allowed[randomnumber].data.title)
-        .setDescription("Posted by: " + allowed[randomnumber].data.author)
-        .setImage(allowed[randomnumber].data.url)
-        .addField("Other info:", "Up votes: " + allowed[randomnumber].data.ups + " / Comments: " + allowed[randomnumber].data.num_comments)
-        .setFooter("Memes provided by r/dankmemes")
-        message.channel.send(embed)
-    } catch (err) {
-        return console.log(err);
-    }
-  }
-});
 
 
 client.login(process.env.BOT_TOKEN)
