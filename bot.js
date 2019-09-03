2228d071555fe372d2aaa0bffa340445a293f8c1
Thomas Helton <thomashelton24@gmail.com>
2019-09-03 08:57:17 -0400

    Random meme?

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
+const snekfetch = require('snekfetch');
 
 require('events').EventEmitter.defaultMaxListeners = 100;
 
@@ -168,7 +169,26 @@ client.on('message', message => {
    }
 });
 
-
+exports.run = async (client, message, args) => {
+    try {
+        const { body } = await snekfetch
+            .get('https://www.reddit.com/r/dankmemes.json?sort=top&t=week')
+            .query({ limit: 800 });
+        const allowed = message.channel.nsfw ? body.data.children : body.data.children.filter(post => !post.data.over_18);
+        if (!allowed.length) return message.channel.send('It seems we are out of fresh memes!, Try again later.');
+        const randomnumber = Math.floor(Math.random() * allowed.length)
+        const embed = new Discord.RichEmbed()
+        .setColor(0x00A2E8)
+        .setTitle(allowed[randomnumber].data.title)
+        .setDescription("Posted by: " + allowed[randomnumber].data.author)
+        .setImage(allowed[randomnumber].data.url)
+        .addField("Other info:", "Up votes: " + allowed[randomnumber].data.ups + " / Comments: " + allowed[randomnumber].data.num_comments)
+        .setFooter("Memes provided by r/dankmemes")
+        message.channel.send(embed)
+    } catch (err) {
+        return console.log(err);
+    }
+}
 
 
 client.login(process.env.BOT_TOKEN)
