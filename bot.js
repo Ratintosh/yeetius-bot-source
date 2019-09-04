4f68749dfffd1f3cc8c6a1d8a1901bfdc9a3b629
Thomas Helton <thomashelton24@gmail.com>
2019-09-04 09:17:52 -0400

    ugh

--- bot.js
+++ bot.js
@@ -185,9 +185,8 @@ client.on('message', message => {
 
 
 client.on('message', message => {
-   if (message.content === ".salute") { //Supposed to delete message
-      message.channel.send(`${message.author} has saluted!`);
-      message.channel.send(`Hello ${user}, and welcome!`);
+   if (message.content === ".salute") {
+      message.channel.send(`${user} has saluted!`);
    }
 });
 
