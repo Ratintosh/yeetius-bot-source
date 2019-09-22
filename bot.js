055f40bcad90c1ecac466424cca7667638a7d977
Thomas Helton <thomashelton24@gmail.com>
2019-09-21 22:25:32 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -240,6 +240,39 @@ client.on('message', message => {
    }
 });
 
+
+
+client.on('message', message => {
+   if (message.content === ".yeet") { //Supposed to delete message
+
+   }
+});
+
+client.on('message', message => {
+   if (message.content === ".yeet") { //Supposed to delete message
+   const exampleEmbed = new Discord.RichEmbed()
+	.setColor('#0099ff')
+	.setTitle('Some title')
+	.setURL('https://discord.js.org/')
+	.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+	.setDescription('Some description here')
+	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
+	.addField('Regular field title', 'Some value here')
+	.addBlankField()
+	.addField('Inline field title', 'Some value here', true)
+	.addField('Inline field title', 'Some value here', true)
+	.addField('Inline field title', 'Some value here', true)
+	.setImage('https://i.imgur.com/wSTFkRM.png')
+	.setTimestamp()
+	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+
+channel.send(exampleEmbed);
+
+}
+});
+
+
+
 //update shit lolx
 
 client.login(process.env.BOT_TOKEN)
